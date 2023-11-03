2a9f8b3d2cbe19936e4c617081f5c5e7cd9fdd99
roboot <roboot@qq.com>
2023-11-03 11:26:23 +0800

    gpt

--- conf/config.py
+++ conf/config.py
@@ -1,7 +1,7 @@
 
 
 # openai的key，语音识别，翻译使用到这个
-open_api_key = "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"
+open_api_key = "you api key"
 
 
 # gpt 翻译的系统预设，可以在这里改语言
@@ -12,7 +12,7 @@ gpt_model = "gpt-3.5-turbo"
 # gpt_model = "gpt-4"
 
 # 微软的文本转语音使用到的key
-tts_subscription_key = '28d827453fc142c28aab5d114d9502c5'
+tts_subscription_key = 'tts  key'
 tts_service_region = 'eastus'
 
 # 需要使用的语音的名字，可以修改声优
